61d68d252b4ad72c357964c1eedde9a4750088a8
DEVDEN-CREATIVE <ashish100rana123@gmail.com>
2020-11-02 17:14:07 +0530

    Updates

---
+++ precache-manifest.ebf1428b5a0eacdc62f14dea20044d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60352eb5bbd852dc659bdab84018559c",
+    "url": "/FacialLandmarkDetection/index.html"
+  },
+  {
+    "revision": "5ddc5e9530bda1652bd6",
+    "url": "/FacialLandmarkDetection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ee2205209e094a1f95a",
+    "url": "/FacialLandmarkDetection/static/js/2.f0b5ab4c.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/FacialLandmarkDetection/static/js/2.f0b5ab4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddc5e9530bda1652bd6",
+    "url": "/FacialLandmarkDetection/static/js/main.297a1688.chunk.js"
+  },
+  {
+    "revision": "4ead208a0ccd937b4c98",
+    "url": "/FacialLandmarkDetection/static/js/runtime-main.733a765a.js"
+  }
+]);
